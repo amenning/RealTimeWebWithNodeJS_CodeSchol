e6550ac7c289873131b727c9785af89e09759ad4
Andrew Menning <menning23@gmail.com>
2016-01-17 16:24:38 -0500

    Part 1 Lesson 6 Updated to serve static jquery file

--- Level6/app.js
+++ Level6/app.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/'));
+
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+
 io.on('connection', function(client){
 	console.log('Client connected...');
-	
+			
 	client.on('join', function(name){
 		client.nickname = name;
 	});
